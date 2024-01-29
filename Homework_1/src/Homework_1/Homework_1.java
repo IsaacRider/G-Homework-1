c3a6d6cc3da6a7e64e6b7e7456c1dd8228b915c5
IsaacRider <imr5y2@umsystem.edu>
2024-01-29 09:43:52 -0600

    Homework_1

---
+++ Homework_1/src/Homework_1/Homework_1.java
@@ -0,0 +1,12 @@
+package Homework_1;
+
+public class Homework_1
+{	
+	public void homework1()
+	{
+		System.out.println("Isaac Rider: UserName: IsaacRider ID: 111994726");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+	}
+}
---
+++ Homework_1/src/Homework_1/Main.java
@@ -0,0 +1,11 @@
+package Homework_1;
+
+public class Main
+{
+	public static void main(String[] args)
+	{
+		Homework_1 Test = new Homework_1();
+		Test.homework1();;
+	}
+
+}
